6ff0dc96f888573b0b8377f87cec4482a0ec799f
HenryPeralta <henrygabrielperalta@gmail.com>
2023-12-28 19:22:46 -0600

    Jirafa vs Oso
    Jirafa vs Oso

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+console.log("Escuchando en el puerto 3000")
+app.listen(3000);
